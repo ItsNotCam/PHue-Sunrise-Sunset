95c7a9f4e034e7f7f41b86b9f76b58b700a57ce4
axiiom <axiiom@u19s.home>
2019-12-07 15:55:45 -0500

    fixed bugs - deployment working as intended

--- setup.sh
+++ setup.sh
@@ -4,8 +4,18 @@ echo "\e[32mInstalling dependencies ..."
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get install docker.io docker-compose python3 python3-pip -y
-python3 -m pip3 install --upgrade pip3
+sudo python3 -m pip3 install --upgrade pip3
 sudo apt-get install python3-venv -y
+echo "\e[32mDone\n"
+
+echo "\e[33mSetting up docker permission for $USER ..."
+sudo groupadd docker >> /dev/null
+sudo usermod -aG docker "$USER" >> /dev/null
+echo "\e[33mDone\n"
+
+echo "\e[33mStarting docker service ... "
+sudo systemctl start docker
+echo "\e[33mDone\n"
 
 echo "\e[36mSetting up virtual environment ... "
 python3 -m venv venv
@@ -17,10 +27,13 @@ pip3 install -r source/requirements.txt
 echo "\e[32mDone\n"
 
 echo "\e[35mSetting up crontab ..."
+CRONSTR="* 3 * * * $PWD/venv/bin/python3 $PWD/source/sunrise_sunset.py # update_sun"
+echo "Creating: <CronItem '$CRONSTR'>"
+(crontab -l ; echo "$CRONSTR") | crontab -
 venv/bin/python3 source/sunrise_sunset.py
 echo "\e[35mDone\n"
 
-echo "\e[34mSetting up docker mysql instance ...\e[0m"
+echo "\e[34mSetting up docker mysql instance ..."
 cd docker
 docker-compose up -d
 echo "\e[34mDone\n"
--- teardown.sh
+++ teardown.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-echo "\e[35mRemoving crontabs relating to this app ... "
+echo "\n\e[35mRemoving crontabs relating to this app ... "
 script='
 from crontab import CronTab
 import sys
 
 user = sys.argv[1]
 cron = CronTab(user=user)
-for comment in ["sunrise","sunset"]:
+for comment in ["sunrise","sunset","update_sun"]:
 	for tab in cron.find_comment(comment):
 		print("Removing:",tab)
 		cron.remove(tab)
@@ -23,3 +23,11 @@ echo "\e[36mDeleting docker instance ...\e[0m"
 cd docker
 docker-compose down
 echo "\e[36mDone\n"
+
+echo "\e[36mRemoving docker image ..."
+docker image rm mysql:8.0.18
+echo "\n\e[36mPruning docker volumes ..."
+docker volume prune -f
+docker volume rm docker_huesql -f
+echo "\e[36mDone\n"
+
